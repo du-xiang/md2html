361fc17f52db6fb41279eee5552ed754657fd47e
woodsoil <2876069311@qq.com>
2026-02-21 12:54:58 +0800

    实现：添加Node类

--- Node.hpp
+++ Node.hpp
@@ -8,7 +8,9 @@ enum class NodeType {
     paragraph,      // 段落
     header,         // 标题
     codeBlock,      // 代码块
+    codeLine,       // 代码行
     mathBlock,      // 数学块
+    mathLine,       // 数学行
     quote,          // 引用
     listContainer,  // 列表容器
     orderList,      // 有序列表
@@ -20,16 +22,18 @@ class Node {
 private:
 
 public:
-    NodeType nodeType;
-    std::string contents;
-    unsigned int layer;
-    std::vector<Node> children;
+    NodeType nodeType;          // 结点类型
+    std::string contents;       // 结点包含的文本内容
+    size_t layer;         // 结点在解析树中的层数（从0开始）
+    size_t level;         // 存放标题、列表、引用的层级
+    std::vector<Node> children; // 存放此结点包含的下层子结点
 
     Node() = default;
     Node(NodeType type);
     ~Node() = default;
     bool set_node_type(NodeType type);
-    bool set_node_layer(unsigned int l);
+    bool set_node_layer(size_t l);
+    bool set_node_level(size_t l);
     bool set_node_contents(std::string c);
 
 };
@@ -43,8 +47,13 @@ inline bool Node::set_node_type(NodeType type) {
     return true;
 }
 
-inline bool Node::set_node_layer(unsigned int l) {
+inline bool Node::set_node_layer(size_t l) {
     layer = l;
+    return true;
+}
+
+inline bool Node::set_node_level(size_t l) {
+    level = l;
     return true;
 }
 
